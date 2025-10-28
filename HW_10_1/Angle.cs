4a6b75a58581a9d5ab9a15462125f17025b3afe0
Administrator <admin@example.com>
2025-10-29 02:13:57 +0300

    Добавьте файлы проекта.

---
+++ HW_10_1/Angle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_10_1
+{
+    class Angle
+    {
+        public int gradus_;
+        public int min_;
+        public int sec_;
+
+        public int Gradus
+        {
+            get { return gradus_; }
+            set
+            {
+                if (value >= 0 && value <= 360)
+                    gradus_ = value;
+                else
+                    throw new ArgumentOutOfRangeException("Градусы должны быть в диапазоне от 0 до 360");
+            }
+        }
+
+        public int Min
+        {
+            get { return min_; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    min_ = value;
+                else
+                    throw new ArgumentOutOfRangeException("Минуты должны быть в диапазоне от 0 до 59");
+            }
+        }
+
+        public int Sec
+        {
+            get { return sec_; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    sec_ = value;
+                else
+                    throw new ArgumentOutOfRangeException("Секунды должны быть в диапазоне от 0 до 59");
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            double totalDegrees = Gradus + Min / 60 + Sec / 3600;
+            return totalDegrees * Math.PI / 180;
+        }
+
+        // Метод для отображения угла в понятном виде
+        public override string ToString()
+        {
+            return $"{Gradus}° {Min}' {Sec}\"";
+        }
+    }
+}
---
+++ HW_10_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+            Console.WriteLine("Введите значение угла!");
+            Console.Write("Введите градусы: ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите угловые минуты: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите угловые секунды: ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus, min, sec);
+
+            Console.WriteLine($"Созданный угол: {angle}");
+            Console.WriteLine($"Значение в радианах: {angle.ToRadians():F6}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено некорректное числовое значение");
+            }
+            Console.ReadKey();
+        }
+    }
+}
